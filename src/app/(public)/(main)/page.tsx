9b5e809029d08cea4ad97858e543a05b7581010b
정찬원 <jcw0522@gachon.ac.kr>
2026-09-18 18:48:11 +0900

    feat: 지역 선택 UI 구현

--- src/app/(public)/(main)/page.tsx
+++ src/app/(public)/(main)/page.tsx
@@ -1,11 +1,13 @@
 import TitleHeader from '@/shared/layout/TitleHeader';
 import styles from './page.module.css';
+import SelectSection from '@/features/main/components/SelectSection';
 
 const RiskAssessmentPage = () => {
   return (
     <div className={styles.page}>
       <div className={styles.panel}>
         <TitleHeader />
+        <SelectSection />
       </div>
     </div>
   );
---
+++ src/features/main/components/SelectSection.tsx
@@ -0,0 +1,47 @@
+import clsx from 'clsx';
+import styles from '../styles/SelectSection.module.css';
+import { textStyles } from '@/app/styles/theme.css';
+import Button from '@/shared/components/button/Button';
+
+const mocks = [
+  '강원특별자치도',
+  '경기도',
+  '경상남도',
+  '경상북도',
+  '광주광역시',
+  '대구광역시',
+  '부산광역시',
+  '서울특별시',
+  '세종특별자치시',
+  '울산광역시',
+  '인천광역시',
+  '전라남도',
+  '전북특별자치도',
+  '충청남도',
+  '충청북도',
+];
+
+const SelectSection = () => {
+  return (
+    <section className={clsx(styles.select_section)}>
+      <article className={clsx(textStyles.subtitle2, styles.option_title)}>
+        <div className={clsx(styles.option_number)}>1</div>
+        <p className={clsx(styles.description)}>지역 선택</p>
+      </article>
+
+      <div className={clsx(styles.option_list, 'thin-scrollbar')}>
+        {mocks.map((item) => (
+          <Button
+            key={item}
+            label={item}
+            variant='selection'
+            size='md'
+            className={styles.option_button}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default SelectSection;
