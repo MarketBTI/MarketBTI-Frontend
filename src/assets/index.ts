aec087a4e3f4eb5ecab282ac5d7775a13922dc9c
정찬원 <jcw0522@gachon.ac.kr>
2026-09-20 02:33:01 +0900

    feat: 유형 캐릭터 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,4 +9,19 @@ export { default as LodingBackgroundIcon } from './icons/LodingBackground.svg';
 export { default as CompleteCircleIcon } from './icons/CompleteCircle.svg';
 
 // character
+export { default as GBFSIcon } from './icons/character/GBFS.svg';
+export { default as GBFVIcon } from './icons/character/GBFV.svg';
+export { default as GBPSIcon } from './icons/character/GBPS.svg';
+export { default as GBPVIcon } from './icons/character/GBPV.svg';
+export { default as GCFSIcon } from './icons/character/GCFS.svg';
 export { default as GCFVIcon } from './icons/character/GCFV.svg';
+export { default as GCPSIcon } from './icons/character/GCPS.svg';
+export { default as GCPVIcon } from './icons/character/GCPV.svg';
+export { default as DBFSIcon } from './icons/character/DBFS.svg';
+export { default as DBFVIcon } from './icons/character/DBFV.svg';
+export { default as DBPSIcon } from './icons/character/DBPS.svg';
+export { default as DBPVIcon } from './icons/character/DBPV.svg';
+export { default as DCFSIcon } from './icons/character/DCFS.svg';
+export { default as DCFVIcon } from './icons/character/DCFV.svg';
+export { default as DCPSIcon } from './icons/character/DCPS.svg';
+export { default as DCPVIcon } from './icons/character/DCPV.svg';
